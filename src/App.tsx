ce12ec8b618cf846721bccd65eab32413edc07a0
secona <77039267+secona@users.noreply.github.com>
2023-12-24 18:33:17 +0700

    add beli perahu form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BeliPerahuForm from "./BeliPerahuForm"
 import Perahu from "./Perahu"
 import getDaftarPerahu from "./api/perahu/getDaftarPerahu"
 import useAPI from "./api/useAPI"
@@ -8,14 +9,17 @@ function App() {
   if (loading) return <h1>Loading...</h1>
 
   return (
-    <div className="flex flex-wrap gap-2">
-      {data && data.daftarPerahu.map(perahu => (
-        <Perahu
-          key={perahu.id}
-          perahu={perahu}
-        />
-      ))}
-    </div>
+    <main>
+      <BeliPerahuForm />
+      <div className="flex flex-wrap gap-2">
+        {data && data.daftarPerahu.map(perahu => (
+          <Perahu
+            key={perahu.id}
+            perahu={perahu}
+          />
+        ))}
+      </div>
+    </main>
   )
 }
 
---
+++ src/BeliPerahuForm.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react"
+import postBeliPerahu, { PostBeliPerahuBody } from "./api/perahu/postBeliPerahu"
+
+export default function BeliPerahuForm() {
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const data = new FormData(event.currentTarget);
+
+    const obj: PostBeliPerahuBody = {
+      name: String(data.get("name")),
+      description: String(data.get("description")),
+      color: String(data.get("color")),
+      capacity: Number(data.get("capacity")),
+    };
+
+    postBeliPerahu(obj).then(() => {
+      setLoading(false);
+      alert("Sukses membeli perahu.");
+      (event.target as any).reset();
+    });
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <table className="border-spacing-x-2 border-separate">
+        <tr>
+          <td className="align-top font-bold">NAME</td>
+          <td>
+            <input
+              disabled={loading}
+              name="name"
+              className="border-b-2 border-solid border-black focus:outline-none"
+              required
+            />
+          </td>
+        </tr>
+        <tr>
+          <td className="align-top font-bold">DESCRIPTION</td>
+          <td>
+            <input
+              disabled={loading}
+              name="description"
+              className="border-b-2 border-solid border-black focus:outline-none"
+              required
+            />
+          </td>
+        </tr>
+        <tr>
+          <td className="align-top font-bold">CAPACITY</td>
+          <td>
+            <input
+              disabled={loading}
+              name="capacity"
+              className="border-b-2 border-solid border-black focus:outline-none"
+              type="number"
+              required
+            />
+          </td>
+        </tr>
+        <tr>
+          <td className="align-top font-bold">COLOR</td>
+          <td>
+            <select
+              disabled={loading}
+              name="color"
+              className="w-full focus:outline-none border-b-2 border-solid border-black"
+              required
+            >
+              <option value="RED">Red</option>
+              <option value="ORANGE">Orange</option>
+              <option value="YELLOW">Yellow</option>
+              <option value="GREEN">Green</option>
+              <option value="BLUE">Blue</option>
+              <option value="INDIGO">Indigo</option>
+              <option value="VIOLET">Violet</option>
+              <option value="WHITE">White</option>
+              <option value="BLACK">Black</option>
+            </select>
+          </td>
+        </tr>
+      </table>
+      <button className="rounded-md border-2 border-solid border-black py-1 px-5 font-semibold">
+        Beli
+      </button>
+    </form>
+  )
+}
